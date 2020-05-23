793c54d97a17b3a2049cb31307d90ca62de464fa
melangyun <melangyun@gmail.com>
2020-05-23 23:37:01 +0900

    how to add ipynb file on git?

---
+++ Algorithm/python - 자료구조 이론.md
@@ -0,0 +1,7 @@
+# python - 자료구조 이론
+
+1. anaconda3 설치
+2. jupyter 설치
+3. jupyter-notebook 실행
+   - browser로 열림
+
---
+++ 인터넷 강의/python_ruby/Mixin/1.rb
@@ -0,0 +1,21 @@
+# ! 루비는 모듈로 다중상속 - mix In 을 지원함
+
+module M1
+    def m1_m
+        p "m1_m"
+    end
+end
+
+module M2
+    def m2_m
+        p "m2_m"
+    end
+end
+
+class C
+    include M1, M2
+end
+
+c = C.new
+c.m1_m
+c.m2_m
